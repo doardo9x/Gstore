61e7ca863c6f83740373b6567739f38c7661eabe
doardo9x <eduardo010304@gmail.com>
2025-02-17 08:02:00 -0300

    Criação da Camada de Dados.

---
+++ GStore/Models/Produto.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GStore.Models;
+
+[Table("produto")]
+public class Produto
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe a Categoria")]
+    public int CategoriaId { get; set; }
+    [ForeignKey("CategoriaId")]
+    public Categoria Categoria { get; set; }
+    
+    [Required(ErrorMessage = "Por favor, informe o Nome")]
+    [StringLength(60, ErrorMessage = "O nome deve possuir no maximo 60 caracteres.")]
+    public string Nome { get; set; }
+    
+    [Display(Name = "Descrição", Prompt = "Descrição")]
+    [StringLength(1000, ErrorMessage = "A descrição deve posssuir no máximo 1000 caracteres.")]
+    public string Descricao { get; set; }
+    
+    [Display(Name = "Quantidade em Estoque")]
+    [Range(0, int.MaxValue)]
+    [Required(ErrorMessage = "Por favor, informe a quantidade de Estoque")]
+    public int QtdeEstoque { get; set; }
+
+    [Display(Name = "Valor de Custo")]
+    [Range(0, double.MaxValue)]
+    [Column(TypeName = "numeric(10,2)")]
+    public decimal ValorCusto { get; set; }
+    
+    [Display(Name = "Valor de Venda")]
+    [Range(0, double.MaxValue)]
+    [Column(TypeName = "numeric(10,2)")]
+    public decimal ValorVenda { get; set; }
+}
